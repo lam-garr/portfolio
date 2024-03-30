26b772839b9bcdd55d78a964eeec889450492618
Garrett <lamgarr@outlook.com>
2024-03-29 22:05:45 -0700

    Add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from './pages/HomePage';
+import Sidebar from './components/Sidebar';
 import './styles/App.css';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage/>}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
