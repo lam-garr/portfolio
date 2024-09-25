1a80adaa1c8f3bcb5984e01ba175dfa5322ca4cf
Garrett <lamgarr@outlook.com>
2024-09-24 20:26:42 -0700

    Edit Home

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
-import './styles/HomePage.css';
+import '../styles/HomePage.css';
 
 function HomePage() {
   return (
-    <></>
+    <main className="home-main">
+      <header className="home-header">
+        <p></p>
+      </header>
+      <section className="home-section-one">
+        <p></p>
+      </section>
+      <section className="home-section-two">
+      </section>
+    </main>
   );
 }
 
