c4a9c80dd9e20bd8ae48eec5d281f004787aa68b
Garrett <lamgarr@outlook.com>
2024-11-23 23:41:32 -0800

    Add Contact component

---
+++ src/components/ContactComponent.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./styles/ContactComponent.css";
+
+function ContactComponent() {
+  return (
+    <section></section>
+  );
+}
+
+export default ContactComponent;
