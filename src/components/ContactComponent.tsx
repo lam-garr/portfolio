a68dc85dff1d59f94e94c43efb5d5b0f5f4fa165
lam-garr <lamgarr@outlook.com>
2024-12-02 23:43:42 -0800

    Add email and github

--- src/components/ContactComponent.tsx
+++ src/components/ContactComponent.tsx
@@ -3,7 +3,16 @@ import "./styles/ContactComponent.css";
 
 function ContactComponent() {
   return (
-    <section></section>
+    <section className="contact-section">
+      <div className="contact-container">
+        <div className="contact-container-email">
+          <text className="email-text">email</text>
+        </div>
+        <div classNmae="contact-container-github">
+          <text className="github-text">github</text>
+        </div>
+      </div>
+    </section>
   );
 }
 
