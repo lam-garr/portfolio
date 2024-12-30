9de2c631c16e2c137c13a56567d2d99cb92aa874
lam-garr <lamgarr@outlook.com>
2024-12-29 21:32:37 -0800

    Add Date component to Home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import DateComponent from '../components/DateComponenet';
 import '../styles/HomePage.css';
 
 function HomePage() {
   return (
     <main className="home-main">
+      <DateComponent/>
       <header className="home-header">
         <p></p>
       </header>
