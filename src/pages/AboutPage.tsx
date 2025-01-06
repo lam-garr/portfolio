110f504f7136aaa5525f6f694977cb565965efa1
lam-garr <lamgarr@outlook.com>
2025-01-05 20:51:06 -0800

    Add Header component to About page

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import HeaderComponent from '../components/HeaderComponent';
 import '../styles/AboutPage.css';
 
 function AboutPage() {
   return (
     <main className="about-main">
+      <HeaderComponent/>
     </main>
   );
 }
