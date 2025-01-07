1adf7fcc94165fffec72d540b06c6f29849c527f
lam-garr <lamgarr@outlook.com>
2025-01-06 22:53:24 -0800

    Add Summary component to About page

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import HeaderComponent from '../components/HeaderComponent';
+import SummaryComponent from "../components/SummaryComponent";
 import '../styles/AboutPage.css';
 
 function AboutPage() {
   return (
     <main className="about-main">
       <HeaderComponent/>
+      <SummaryComponent/>
     </main>
   );
 }
