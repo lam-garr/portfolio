53412f5e4db2e1f9154b627f20d66ef0233795af
lam-garr <lamgarr@outlook.com>
2025-01-07 19:50:35 -0800

    Add Project component to Project page

--- src/pages/ProjectPage.tsx
+++ src/pages/ProjectPage.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import HeaderComponent from '../components/HeaderComponent';
+import ProjectComponent from '../components/ProjectComponent';
 import "./styles/ProjectPage.css";
 
 function ProjectPage() {
   return (
     <main className="project-main">
       <HeaderComponent/>
+      <ProjectComponent/>
     </main>
   );
 }
