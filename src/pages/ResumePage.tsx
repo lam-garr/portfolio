cc00bd5e1faef1e06dc262569ef86c8d4b80589d
lam-garr <lamgarr@outlook.com>
2025-01-08 22:45:29 -0800

    Add Resume component to Resume page

--- src/pages/ResumePage.tsx
+++ src/pages/ResumePage.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import HeaderComponent from '../components/HeaderComponent';
+import ResumeComponent from "../components/ResumeComponent";
 import '../styles/ResumePage.css';
 
 function ResumePage() {
   return (
     <main className="resume-main">
       <HeaderComponent/>
+      <ResumeComponent/>
     </main>
   );
 }
